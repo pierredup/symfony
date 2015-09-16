bb7ac743b31b74d948f0c3bb6ee30eec4eb68eb1
Pierre du Plessis <pierre@pcservice.co.za>
2015-09-16 14:15:29 +0200

    Updates

--- src/Symfony/Bundle/FrameworkBundle/Tests/Controller/ControllerResolverTest.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/Controller/ControllerResolverTest.php
@@ -113,6 +113,7 @@ class ControllerResolverTest extends BaseControllerResolverTest
      */
     public function testGetControllerOnNonUndefinedFunction($controller, $exceptionName = null, $exceptionMessage = null)
     {
+        $this->setExpectedException($exceptionName, $exceptionMessage);
         parent::testGetControllerOnNonUndefinedFunction($controller, $exceptionName, $exceptionMessage);
     }
 
@@ -123,9 +124,9 @@ class ControllerResolverTest extends BaseControllerResolverTest
             array('foo::bar', '\InvalidArgumentException', 'Class "foo" does not exist.'),
             array('stdClass', '\LogicException', 'Unable to parse the controller name "stdClass".'),
             array(
-                'Symfony\Component\HttpKernel\Tests\Controller\ControllerTest::bar',
+                'Symfony\Component\HttpKernel\Tests\Controller\ControllerResolverTest::bar',
                 '\InvalidArgumentException',
-                'The controller for URI "/" is not callable. Expected method "bar" on class "Symfony\Component\HttpKernel\Tests\Controller\ControllerTest". Available methods: "publicAction", "staticAction"',
+                'Controller "Symfony\\Component\\HttpKernel\\Tests\\Controller\\ControllerResolverTest::bar" for URI "/" is not callable.',
             ),
         );
     }
